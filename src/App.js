66021128b7db828e0221d5a10851c70517c3a93d
SamanthaF <samantha.foigel@gmail.com>
2024-06-04 19:40:10 -0400

    adding css styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Hero from "../src/components/Hero.js"
+import Map from "../src/components/Map.js"
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-    
+      <Hero />
+      <Map />
     </div>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+export default function Hero() {
+    return (
+        <>
+            <header class="masthead">
+                <div class="container position-relative">
+                    <div class="row justify-content-center">
+                        <div class="col-xl-6">
+                            <div class="text-center text-white">
+                                <h1 class="mb-5">Welcome to TTC Elevator Tracker</h1>
+                                <p>Track elevator statuses at TTC stations in real-time.</p>
+                                <div class="search-input">
+                                    <input type="text" class="form-control form-control-lg" placeholder="Enter a station"></input>
+                                    <button type="submit" class="btn btn-primary btn-lg disabled">Search</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </>
+    )
+}
