1c0ae135ece35d2bd898f591d9811c4fc3693e77
SamanthaF <samantha.foigel@gmail.com>
2024-06-05 13:55:06 -0400

    map updates

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import React from "react";
+import { MapContainer, ImageOverlay, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import useElevatorData from "./UseElevatorData.js"
+
 import Hero from "../src/components/Hero.js"
 import Map from "../src/components/Map.js"
 import './App.css';
