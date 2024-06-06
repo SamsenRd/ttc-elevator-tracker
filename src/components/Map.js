1fe614d60bb9c3597c583dd80ced333f51380924
SamanthaF <samantha.foigel@gmail.com>
2024-06-06 16:08:24 -0400

    added a map with css

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,55 +1,110 @@
 import React from "react"
-import { useEffect } from 'react';
-import { MapContainer, TileLayer, useMap } from 'react-leaflet';
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-import '../';
+import "../styles/Map.css"
+import ttcImg from "../images/TTC_Subway_Map.png"
 
-const imageUrl = '../src/images/TTC_SubwayStreetcarMap.jpg';
-const errorOverlayUrl = 'https://cdn-icons-png.flaticon.com/512/110/110686.png';
-const altText = 'Toronto TTC Subway and Streetcar Map';
-const latLngBounds = L.latLngBounds([
-    [43.855, -79.6], // Adjust these coordinates based on the actual bounds of your image
-    [43.58, -79.12]
-]);
-
-function ImageOverlayComponent() {
-    const map = useMap();
-
-    useEffect(() => {
-        const imageOverlay = L.imageOverlay(imageUrl, latLngBounds, {
-            opacity: 0.8,
-            errorOverlayUrl: errorOverlayUrl,
-            alt: altText,
-            interactive: true
-        }).addTo(map);
-
-        L.rectangle(latLngBounds).addTo(map); // Optional: to show the bounds
-        map.fitBounds(latLngBounds);
-
-        return () => {
-            map.removeLayer(imageOverlay);
-        };
-    }, [map]);
-
-    return null;
-}
 
 export default function Map() {
     return (
         <>
-            <MapContainer
-                center={[43.7, -79.4]}  // Adjust the initial center of the map
-                zoom={12}
-                style={{ height: "100vh", width: "100%" }}
-            >
-                <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                    maxZoom={19}
-                />
-                <ImageOverlayComponent />
-            </MapContainer>
+            <div className="distribution-map">
+                <img src={ttcImg} alt="TTC Subway and Streetcar Map" />
+                <button className="map-point" style={{ top: "8.2%", left: "22.5%" }}>
+                    <div className="content">
+                        <div className="centered-y">
+                            <h2>A Place</h2>
+                            <p>You can put plenty of details in here. In the original, I listed contact information and linked phone numbers and email addresses.</p>
+                        </div>
+                    </div>
+                </button>
+                <button className="map-point" style={{ top: "12.5%", left: "22.5%" }}>
+                    <div className="content">
+                        <div className="centered-y">
+                            <h2>Another Place</h2>
+                            <p>Lorem ipsum something something</p>
+                        </div>
+                    </div>
+                </button>
+                <button className="map-point" style={{ top: "16%", left: "25%" }}>
+                    <div className="content">
+                        <div className="centered-y">
+                            <h2>Marauder Town</h2>
+                            <p>I solemnly swear that I am up to no good</p>
+                        </div>
+                    </div>
+                </button>
+                <button className="map-point" style={{ top: "19.1%", left: "27.6%" }}>
+                    <div className="content">
+                        <div className="centered-y">
+                            <h2>Logan's Mum</h2>
+                            <p>I&rsquo;m a marshmallow</p>
+                        </div>
+                    </div>
+                </button>
+                <button className="map-point" style={{ top: "22.5%", left: "30.1%" }}>
+                    <div className="content">
+                        <div className="centered-y">
+                            <h2>Toto</h2>
+                            <p>I bless the rains</p>
+                        </div>
+                    </div>
+                </button>
+                <button className="map-point" style={{ top: "25.7%", left: "32.7%" }}>
+                    <div className="content">
+                        <div className="centered-y">
+                            <h2>With Love</h2>
+                            <p>&mdash; 007</p>
+                        </div>
+                    </div>
+                </button>
+                <button className="map-point" style={{ top: "28.8%", left: "35.2%" }}>
+                    <div className="content">
+                        <div className="centered-y">
+                            <h2>A Place</h2>
+                            <p>You can put plenty of details in here. In the original, I listed contact information and linked phone numbers and email addresses.</p>
+                        </div>
+                    </div>
+                </button>
+                <button className="map-point" style={{ top: "33.3%", left: "35.2%" }}>
+                    <div className="content">
+                        <div className="centered-y">
+                            <h2>Another Place</h2>
+                            <p>Lorem ipsum something something</p>
+                        </div>
+                    </div>
+                </button>
+                <button className="map-point" style={{ top: "36.3%", left: "35.2%" }}>
+                    <div className="content">
+                        <div className="centered-y">
+                            <h2>Marauder Town</h2>
+                            <p>I solemnly swear that I am up to no good</p>
+                        </div>
+                    </div>
+                </button>
+                <button className="map-point" style={{ top: "39%", left: "35.2%" }}>
+                    <div className="content">
+                        <div className="centered-y">
+                            <h2>Logan's Mum</h2>
+                            <p>I&rsquo;m a marshmallow</p>
+                        </div>
+                    </div>
+                </button>
+                <button className="map-point" style={{ top: "44.6%", left: "35.2%" }}>
+                    <div className="content">
+                        <div className="centered-y">
+                            <h2>Toto</h2>
+                            <p>I bless the rains</p>
+                        </div>
+                    </div>
+                </button>
+                <button className="map-point" style={{ top: "51.6%", left: "40.6%" }}>
+                    <div className="content">
+                        <div className="centered-y">
+                            <h2>With Love</h2>
+                            <p>&mdash; 007</p>
+                        </div>
+                    </div>
+                </button>
+            </div>
         </>
     )
 }
