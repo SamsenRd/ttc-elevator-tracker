b2b8b079ab12f22b05016e84d882f6090243fd99
SamanthaF <samantha.foigel@gmail.com>
2024-06-20 16:24:17 -0400

    updates

--- src/App.mjs
+++ src/App.mjs
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Hero from "./components/Hero.mjs";
 import Map from "./components/Map.mjs";
 import "./App.css";
 
 export default function App() {
+ 
 
   return (
     <div className="App">
--- src/components/Map.mjs
+++ src/components/Map.mjs
@@ -6,30 +6,6 @@ import redIcon from "../images/red-icon.png"
 import greenIcon from "../images/green-icon.png"
 
 export default function Map() {
-
-  // Function to close the popup
-  const closePopup = () => {
-    const popup = document.getElementById("popup")
-    popup.classList.remove("show")
-  }
-
-  useEffect(() => {
-    const handleResize = () => {
-      const popup = document.getElementById("popup")
-      if(window.innerWidth <= 768){
-        popup.classList.add("show")
-      }else{
-        popup.classList.remove("show")
-      }
-    }
-    handleResize()
-    window.addEventListener("resize", handleResize)
-    
-    return() => {
-      window.removeEventListener("resize", handleResize)
-    }
-  }, [])
-
   const [stations, setStations] = useState([
     { top: "8.2%", left: "22.5%", name: "Vaughan Metropolitan Centre", status: "In service" },
     { top: "12.5%", left: "22.5%", name: "Highway 407", status: "In service" },
@@ -119,14 +95,6 @@ export default function Map() {
 
   return (
     <>
-    {/* popup */}
-      <div id="popup" className="popup">
-        <div className="popup-content">
-          <h2>Turn your phone sideways for optimal viewing</h2>
-          <button onClick={closePopup}>Got it</button>
-        </div>
-      </div>
-
       <div className="mapHeader">
         <h2>Real-time Elevator Updates</h2>
         <div className="mapLegend">
